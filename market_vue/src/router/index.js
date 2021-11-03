0ae2db6ca3c855a8f02d396bacf948f1987a36a4
jigsaw <misho_1998@mail.ru>
2021-11-03 20:44:40 +0300

    add signup

--- market_vue/src/router/index.js
+++ market_vue/src/router/index.js
@@ -6,6 +6,7 @@ import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
 
 const routes = [
   {
@@ -16,9 +17,6 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
@@ -40,6 +38,11 @@ const routes = [
     path: '/cart',
     name: 'Cart', 
     component: Cart
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp', 
+    component: SignUp
   }
 ]
 
