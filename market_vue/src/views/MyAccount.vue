c956df9065278d4f55fe3407780182ad1a13750a
jigsaw <misho_1998@mail.ru>
2021-11-04 19:14:03 +0300

    add orders

--- market_vue/src/views/MyAccount.vue
+++ market_vue/src/views/MyAccount.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="page-my-account">
+    <div class="page-my-account">
         <div class="columns is-multiline">
             <div class="column is-12">
                 <h1 class="title">My account</h1>
@@ -25,19 +25,42 @@
 
 <script>
 import axios from 'axios'
+import OrderSummary from '@/components/OrderSummary.vue'
 export default {
-	name: 'MyAccount',
-	methods: {
-		logout() {
+    name: 'MyAccount',
+    components: {
+        OrderSummary
+    },
+    data() {
+        return {
+            orders: []
+        }
+    },
+    mounted() {
+        document.title = 'My account | Djackets'
+        this.getMyOrders()
+    },
+    methods: {
+        logout() {
             axios.defaults.headers.common["Authorization"] = ""
             localStorage.removeItem("token")
             localStorage.removeItem("username")
             localStorage.removeItem("userid")
-
             this.$store.commit('removeToken')
-
             this.$router.push('/')
         },
-	}
+        async getMyOrders() {
+            this.$store.commit('setIsLoading', true)
+            await axios
+                .get('/api/v1/orders/')
+                .then(response => {
+                    this.orders = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+            this.$store.commit('setIsLoading', false)
+        }
+    }
 }
 </script>
